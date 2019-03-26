733b29dff2c8a73b06ba8364be378c3ae25a2f50
NanoClem <clemdecoop.pro@gmail.com>
2019-03-26 17:10:41 +0100

    Tests des méthode de l'API OpenFoodFacts

---
+++ main.py
@@ -0,0 +1,54 @@
+import openfoodfacts as openFF
+
+
+
+def printAllergens(country) :
+    """
+    Affiche les allergenes selon le pays
+    PARAM country : code pays
+    """
+    allergens = openFF.facets.get_allergens()       # allergenes disponibles
+    country += ":"
+    for prod in allergens :
+        if country in prod['name'] :
+            print(prod)
+
+
+def printProductsByAllergen(allergen) :
+    """
+    Affiche les produits contenant
+    l'allergene specifique
+    PARAM allergen : id de l'allergene
+    """
+    products = openFF.products.get_by_allergen(allergen)
+    for prod in products :
+        print("==========================================================")
+        print("################## PRODUIT SUIVANT ##################")
+        for key,value in prod.items() :
+            print(key + ":", value)
+        print("==========================================================")
+        break
+
+
+def printProducts(country) :
+    """
+    Affiche les éléments du produit selon le pays
+    PARAM country : code pays
+    """
+    products = openFF.products.get_by_language("fr")
+    for key,value in products[0].items() :
+        print(key + ":", value)
+
+
+
+
+def main() :
+    country = "fr"
+    #printAllergens(country)
+    #printProducts(country)
+    printProductsByAllergen("fr:preparation-de-fromage-fondu")
+
+
+
+if __name__ == '__main__':
+    main()
